ee44c35cf37f6205a7e58b00f9d40ed2efaa2975
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-04-17 17:45:48 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.get('/', (req, res) => {
   // 拿到全部的 todo 資料
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos })) // 將資料傳給 index.hbs
     .catch(error => console.log(error))
 })
