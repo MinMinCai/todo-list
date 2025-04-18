80bc0ea0ed15779b524151e2c9fc35d540516227
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-04-18 17:25:37 +0800

    feat: add bootstrap & modify main.hbs

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose')
 
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const app = express()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })  // 設定連線到mongoDB
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
 const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -31,8 +19,6 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000.')
