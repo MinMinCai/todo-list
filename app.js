d074d6ae961636f6fd944d79d7287c23ffc54129
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-06-23 17:54:03 +0800

    feat: add message partialsfeat: add message partials

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
@@ -30,10 +31,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   // console.log(req.user)
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
